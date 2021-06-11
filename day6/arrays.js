6fde3f85287c2d018c88bf97246e29780c81f411
Yashvi <yashvigarg330@gmail.com>
2021-06-12 00:41:36 +0530

    day6

--- day6/arrays.js
+++ day6/arrays.js
@@ -24,12 +24,26 @@ console.log(first([7, 9, 0, -2], 3));
 console.log(first([7, 9, 0, -2], 6));
 console.log(first([7, 9, 0, -2], -3));
 //4.Write a simple JavaScript program to join all elements of the following array into a string.
-function function4() {
-  var myColor = ["Red", "Green", "White", "Black"];
-  var r = myColor.join(",");
-  var s = myColor.join("+");
-  console.log(r);
-  console.log(s);
-}
+let myColor = ["Red", "Green", "White", "Black"];
+let p = myColor.join(",");
+let q = myColor.join("+");
+console.log(p);
+console.log(q);
 //5. Write a JavaScript program to find the most frequent item of an array
-function mostFreq() {}
+var arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+y = arr1.sort();
+var uniqueNums = arr1.filter((x, i, a) => a.indexOf(x) == i);
+console.log(y);
+console.log(uniqueNums);
+let maxn = 0;
+for (i = 0; i < uniqueNums.length; i++) {
+  let a = arr1.indexOf(uniqueNums[i]);
+  let b = arr1.lastIndexOf(uniqueNums[i]);
+  let x = b - a + 1;
+  if (maxn < x) {
+    maxn = x;
+    n = uniqueNums[i];
+  }
+}
+console.log(n + " = " + maxn + " times");
+//console.log(`${n} = ${maxn} times`); template literals ES6 FEATURE
