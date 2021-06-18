954cecc37ad86348af7c0d7af7a172a2ceaf2ff2
Yashvi <yashvigarg330@gmail.com>
2021-06-18 22:58:44 +0530

    done

--- day9/project/day9.js
+++ day9/project/day9.js
@@ -1,21 +1,12 @@
+let seats = 0;
 const rectangles = document.querySelectorAll(".rectangle");
-document.getElementById("booked").innerHTML = "Booked Seats: 0";
-document.getElementById("remaining").innerHTML = "Remaining Seats: 36";
-let bookedCount = 0,
-  remainingCount = 36;
 Array.from(rectangles).forEach((rectangle) => {
-  rectangle.addEventListener("click", () => {
-    if (rectangle.style.background == "rgb(92,209,230)") {
-      rectangle.style.background = "rgb(255, 255, 255)";
-      bookedCount += 1;
-      remainingCount -= 1;
-    } else {
-      rectangle.style.background = "rgb(92, 209, 230)";
-      bookedCount -= 1;
-      remainingCount += 1;
-    }
-
-    booked.innerHTML = "Booked Seats : " + bookedCount;
-    remaining.innerHTML = "Remaining Seats : " + remainingCount;
+  rectangle.addEventListener("click", (x) => {
+    let result = rectangle.classList.toggle("active");
+    if (result) seats++;
+    else seats--;
+    document.querySelector("#booked").innerHTML = "Booked Seats : " + seats;
+    const remaining = (document.querySelector("#remaining").innerHTML =
+      36 - seats);
   });
 });
