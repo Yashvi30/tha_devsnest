44d651d9a1647f948a665e4e46ee3fafa5f207d8
Yashvi <yashvigarg330@gmail.com>
2021-06-27 04:51:42 +0530

    done

--- day 12/script.js
+++ day 12/script.js
@@ -1,22 +1,30 @@
 let addTask = document.getElementById("addTask");
 let todo = document.getElementById("todo");
 let inputTask = document.getElementById("inputTask");
-let del = document.getElementById("delete");
-let done = document.getElementById("done");
+let ulelement = document.getElementById("ule");
 
 addTask.addEventListener("click", function () {
-  var paragraph = document.createElement("li");
+  let item = `<li>
+  ${inputTask.value}
+  <button id="done" onclick="done(this)">
+    <img
+      src="https://img.icons8.com/material-outlined/24/000000/checked-2--v1.png"
+    />
+  </button>
 
-  paragraph.classList.add("paragraph-styling");
-  paragraph.innerText = inputTask.value;
-  todo.appendChild(paragraph);
-  // todo.appendChild(done);
-  // todo.appendChild(del);
+  <button id="delete" onclick="del(this)">
+    <img
+      src="https://img.icons8.com/ios-glyphs/24/000000/delete-forever.png"
+    />
+  </button>
+</li>`;
+  ulelement.innerHTML += item;
   inputTask.value = "";
-  done.addEventListener("click", function () {
-    paragraph.style.textDecoration = "line-through";
-  });
-  del.addEventListener("click", function () {
-    todo.removeChild(paragraph);
-  });
 });
+
+done = (x) => {
+  x.parentElement.style.textDecoration = "line-through";
+};
+del = (x) => {
+  x.parentElement.innerHTML = "";
+};
