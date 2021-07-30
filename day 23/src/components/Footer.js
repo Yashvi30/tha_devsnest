fd25858bd50bacc8006fd696ff58cd1f97281128
Yashvi <yashvigarg330@gmail.com>
2021-07-30 19:00:49 +0530

    done

--- day 23/src/components/Footer.js
+++ day 23/src/components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer-container">
-      <p>Copyright &copy; 2021 codeantik. All rights Reserved</p>
+      <p>Copyright &copy; 2021 All rights Reserved</p>
     </footer>
   );
 };
