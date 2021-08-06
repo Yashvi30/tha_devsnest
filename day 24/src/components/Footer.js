5588e13083ab5f8ea7d5a7d52979224bf68dba03
Yashvi <yashvigarg330@gmail.com>
2021-08-06 22:29:34 +0530

    done

---
+++ day 24/src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <p>Copyright &copy; 2021 All rights Reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
